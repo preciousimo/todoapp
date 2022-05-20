7a27d89ed88bf8cc844f66ff48a938cdbd64cf92
preciousimo <preciousimoniakemu@gmail.com>
2022-05-20 04:42:08 +0100

    Other CRUD Operations

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -20,3 +20,11 @@ class TodoListCreate(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # usercan only update, delete own posts
+        return Todo.objects.filter(user=user)
