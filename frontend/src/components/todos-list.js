5578eea1403a78c749bee11e7f5da75d501bf7ee
preciousimo <preciousimoniakemu@gmail.com>
2022-05-21 20:36:15 +0100

    Completing Todo

--- frontend/src/components/todos-list.js
+++ frontend/src/components/todos-list.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import TodoDataService from '../services/todos';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Alert from 'react-bootstrap/Alert'
-import moment from 'moment';
+import TodoDataService from "../services/todos";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import moment from "moment";
 
-const TodosList = props => {
+const TodosList = (props) => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
@@ -16,31 +16,43 @@ const TodosList = props => {
 
   const retrieveTodos = () => {
     TodoDataService.getAll(props.token)
-    .then(response => {
-      setTodos(response.data);
-    })
-    .catch( e => {
-      console.log(e);
-    });
-  }
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-  const deleteTodo = (todoId) =>{
+  const deleteTodo = (todoId) => {
     TodoDataService.deleteTodo(todoId, props.token)
-    .then(response => {
-      retrieveTodos();
-    })
-    .catche(e =>{
-      console.log(e)
-    });
-  }
+      .then((response) => {
+        retrieveTodos();
+      })
+      .catche((e) => {
+        console.log(e);
+      });
+  };
+
+  const completeTodo = (todoId) => {
+    TodoDataService.completeTodo(todoId, props.token)
+      .then((response) => {
+        retrieveTodos();
+        console.log("completeTodo", todoId);
+      })
+      .catche((e) => {
+        console.log(e);
+      });
+  };
 
   return (
     <Container>
-      {props.token==null||props.token===""?(
-        <Alert variant='warning'>
-          You are not logged in. Please <Link to={"/login"}>Login</Link> to see your todos.
+      {props.token == null || props.token === "" ? (
+        <Alert variant="warning">
+          You are not logged in. Please <Link to={"/login"}>Login</Link> to see
+          your todos.
         </Alert>
-      ):(
+      ) : (
         <div>
           <Link to={"/todos/create"}>
             <Button variant="outline-info" className="mb-3">
@@ -48,31 +60,54 @@ const TodosList = props => {
             </Button>
           </Link>
           {todos.map((todo) => {
-        return (
-          <Card key={todo.id} className="mb-3">
-            <Card.Body>
-              <div>
-                <Card.Title>{todo.title}</Card.Title>
-                <Card.Text><b>Memo:</b>{todo.memo}</Card.Text>
-                <Card.Text>Date created: {moment(todo.created).format("Do MMMM YYYY")}</Card.Text>
-              </div>
-              <Link to={{
-                pathname: "/todos/" + todo.id,
-                state: {
-                  currentTodo:todo
-                }
-              }}>
-                <Button variant="outline-info" className="me-2">
-                  Edit
-                </Button>
-              </Link>
-              <Button variant="outline-danger" onClick={() => deleteTodo(todo.id)}>
-                Delete
-              </Button>
-            </Card.Body>
-          </Card>
-        )
-      })}
+            return (
+              <Card key={todo.id} className="mb-3">
+                <Card.Body>
+                  <div
+                    className={`${
+                      todo.completed ? "text-decoration-line-through" : ""
+                    }`}
+                  >
+                    <Card.Title>{todo.title}</Card.Title>
+                    <Card.Text>
+                      <b>Memo:</b> {todo.memo}{" "}
+                    </Card.Text>
+                    <Card.Text>
+                      Date created:{" "}
+                      {moment(todo.created).format("Do MMMM YYYY")}
+                    </Card.Text>
+                  </div>
+                  {!todo.completed && (
+                    <Link
+                      to={{
+                        pathname: "/todos/" + todo.id,
+                        state: {
+                          currentTodo: todo,
+                        },
+                      }}
+                    >
+                      <Button variant="outline-info" className="me-2">
+                        Edit
+                      </Button>
+                    </Link>
+                  )}
+                  <Button
+                    variant="outline-danger"
+                    onClick={() => deleteTodo(todo.id)}
+                    className="me-2"
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    variant="outline-success"
+                    onClick={() => completeTodo(todo.id)}
+                  >
+                    Complete
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </div>
       )}
     </Container>
